2ef754150a7da725b1a2b1006f0e27dc3bdd9565
aleksander-nikolaenko <41318656+aleksander-nikolaenko@users.noreply.github.com>
2022-06-22 16:23:57 +0000

    Deploying to gh-pages from @ aleksander-nikolaenko/goit-react-hw-08-phonebook@82afe337cfcefaa2e18da97178c3be51c31d595e 🚀

---
+++ static/js/home-page.31ad99cd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[223],{2035:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184),r=function(){return(0,t.jsx)("div",{children:" Home Page "})}}}]);
+//# sourceMappingURL=home-page.31ad99cd.chunk.js.map
